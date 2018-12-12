9ba3ffc892a0a026e96f3ea2ac7747a572e8b3d6
Arun Jeevanantham <itsmearun98@gmail.com>
2018-12-11 22:00:31 -0800

    first commit

---
+++ src/LaunchScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './LaunchScreen.css';
+
+class LaunchScreen extends Component {
+  render() {
+    return (
+      <div className="test">
+        <p> Hello World!</p>
+      </div>
+    );
+  }
+}
+
+export default LaunchScreen;
