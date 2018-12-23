7d85b83d6b04e30b376cc390f0a6a1ae2172005f
yasz24 <yash17shetty@gmail.com>
2018-12-23 18:09:00 +0530

    launch screen with start button

--- src/LaunchScreen.js
+++ src/LaunchScreen.js
@@ -2,12 +2,38 @@ import React, { Component } from 'react';
 import './LaunchScreen.css';
 import Grid from './Grid/Grid.js'
 
+
+function StartButton(props) {
+	return(
+		<button onClick={props.onClick}>
+		Start Game
+		</button>);
+}
+
+function handleStartClick() {
+	this.setState({gameStarted: true})
+}
+
 class LaunchScreen extends Component {
-  render() {
-    return (
-      <Grid/>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {gameStarted: false}
+	}
+	render() {
+		const startClicked = this.state.gameStarted;
+		let screen;
+
+		if (startClicked) {
+			screen = <Grid/>
+		} else {
+			screen = <StartButton onClick={this.handleStartClick}/>;
+		}
+		return (
+			<div>
+				{screen}
+			</div>
+		);
+	}
 }
 
 export default LaunchScreen;
