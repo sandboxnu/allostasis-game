095cac787b5ccbf046d2f67b2394a1cb6dc6c340
Arun Jeevanantham <itsmearun98@gmail.com>
2018-12-25 22:48:01 -0500

    Pulled in LaunchScreen and updated with GameController

--- src/LaunchScreen.js
+++ src/LaunchScreen.js
@@ -1,13 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component, Button } from 'react';
 import './LaunchScreen.css';
-import GameController from './GameController.js'
+import GameController from './GameController';
+
+
 
 class LaunchScreen extends Component {
-  render() {
-    return (
-      <GameController/>
-    );
+	constructor(props) {
+		super(props);
+    this.state = {gameStarted: false};
+	}
+
+	startButton() {
+		return(
+		<button onClick={this.handleStartClick}>
+			Start Game
+		</button>);
+	}
+
+	handleStartClick = () => {
+		this.setState({gameStarted: true});
   }
+
+	render() {
+		const startClicked = this.state.gameStarted;
+		let screen;
+
+		if (startClicked) {
+			screen = <GameController/>
+		} else {
+			screen = this.startButton();
+		}
+		return (
+			<div className="launchScreen">
+				{screen}
+			</div>
+		);
+	}
 }
 
 export default LaunchScreen;
