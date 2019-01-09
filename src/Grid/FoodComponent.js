b01db220391eec08fb5cbb169d8515dc8ce30a80
Arun Jeevanantham <itsmearun98@gmail.com>
2019-01-08 21:43:26 -0500

    updated the UI for the water/food grid

--- src/Grid/FoodComponent.js
+++ src/Grid/FoodComponent.js
@@ -1,10 +1,42 @@
+import React from 'react';
 import GridComponent from './GridComponent.js'
 import GlobalConstants from '../GlobalConstants';
+import ConfigurableValuesController from '../ConfigurableValuesController.js';
+import './GridComponent.css';
 
 class FoodComponent extends GridComponent {
 
   componentWillMount() {
     this.label = this.props.foodType === GlobalConstants.FOOD_ONE_GRID_CELL ? 'F1' : 'F2';
+    this.background = this.props.foodType === GlobalConstants.FOOD_ONE_GRID_CELL ? ConfigurableValuesController.getFoodOneImage() : ConfigurableValuesController.getFoodTwoImage();
+    if (!this.shouldRenderImage()){
+      this.extraStyle = {
+        backgroundColor: '#10b301',
+      }
+    };
+
+  }
+
+  renderBackground() {
+    if (this.shouldRenderImage()){
+      let bgImg = this.background.startsWith('assets') ? require("../" +this.background) : this.background;
+      
+      return (
+        <div className="gridImage">
+          <img src={bgImg} alt="" className="profileImg" />
+        </div>
+      );
+    }
+
+    return (
+      <div className="gridLabel">
+        {this.label}
+      </div>
+    );
+  }
+
+  shouldRenderImage() {
+    return this.background != null;
   }
 
 }
