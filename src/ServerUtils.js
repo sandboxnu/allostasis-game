156dd9220385e9ffcadec293f38634dd5b14c271
Arun Jeevanantham <itsmearun98@gmail.com>
2019-04-02 13:47:46 -0400

    Changing server back to test

--- src/ServerUtils.js
+++ src/ServerUtils.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 import React from 'react';
 
 
-const SERVER_URL = 'https://api.sandboxneu.com/authtest/';
+const SERVER_URL = 'https://api.sandboxneu.com/test/';
 
 class ServerUtils {
   sendData(collected) {
